49390ffb46b49d2afa0cc71d140712008778f4ef
VScoldness <yyao26@crimson.ua.edu>
2024-06-03 14:51:06 -0700

    add new function of combining database

--- pyxtal_ff/utilities/data2db.py
+++ pyxtal_ff/utilities/data2db.py
@@ -5,23 +5,46 @@ from monty.serialization import loadfn
 from tqdm import tqdm
 
 def outcar2db_list(outcar_path_list, db_path, group="Surface"):
+    wrong_files = []
     with connect(db_path) as _database:
         for cur_outcar in tqdm(outcar_path_list):
             try:
                 cur_structure , dat = outcar2db(cur_outcar, group)
-                _database.write(cur_structure, data=dat)
             except:
-                print("Wrong data: ", cur_outcar)
+                print("\nWrong data: ", cur_outcar)
+                wrong_files.append(cur_outcar)
+                continue
+            finally:
+                _database.write(cur_structure, data=dat)
+    print(len(wrong_files))
 
 
 def outcar2db(cur_outcar, group="Surface"):
     cur_structure = read(cur_outcar)
+    energy = -1 * cur_structure.get_stress()/units.GPa
+    energy[-1], energy[-3] = energy[-3], energy[-1]
     dat = {'energy': cur_structure.get_potential_energy(),
             'force':  cur_structure.get_forces(),
-            'stress': cur_structure.get_stress()/units.GPa,
+            'stress': energy,
             'group':  group}
+    print(cur_structure)
+    print(energy)
+    print(cur_structure.get_forces())
     return cur_structure, dat
 
+def combineDB(output: str, combined_bds: list[str]):
+    with connect(output) as _database:
+        for bd in combined_bds:
+            with connect(bd) as database:
+                for row in database.select():
+                    structure = database.get_atoms(row.id)
+                    dat = {'energy': row.data['energy'],
+                        'force': row.data['force'],
+                        'stress': row.data['stress'],
+                        'group': row.data['group']}
+                    _database.write(structure, data=dat)
+
+        
 
 def json2db(json_path_list, db_path, group="NoElastic"):
     with connect(db_path) as _database:
