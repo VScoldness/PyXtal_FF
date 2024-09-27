e399221076dd89b8ee2cc02d7705589668fd5e61
VScoldness <yyao26@crimson.ua.edu>
2024-09-27 13:50:45 -0700

    update displace precision and change the loss function back

--- pyxtal_ff/models/neuralnetwork.py
+++ pyxtal_ff/models/neuralnetwork.py
@@ -232,7 +232,7 @@ class NeuralNetwork():
                     print("Initial state : ")
                     def closure(): # LBFGS gets loss and its gradient here.
                         train_loss, E_mae, F_mae, S_mae = self.calculate_loss(self.models, self.data)
-                        print("    Loss: {:10.6f}     Energy MAE: {:10.4f}     Force MAE: {:10.4f}     Stress MAE: {:10.4f}".\
+                        print("    Loss: {:10.8f}     Energy MAE: {:10.6f}     Force MAE: {:10.6f}     Stress MAE: {:10.6f}".\
                                 format(train_loss, E_mae, F_mae, S_mae))
                         self.optimizer.zero_grad()
                         train_loss.backward()
@@ -473,12 +473,9 @@ class NeuralNetwork():
         # output = [[cur_energy_loss, cur_force_loss, cur_stress_loss, cur_energy_mae, cur_force_mae, cur_stress_mae, n_atoms, cur_count], ...]
         output = self._sum_together(output)
         
-        # energy_loss = output[0] / (2. * len(batch))
-        # force_loss  = output[1] / (2. * output[-2]) if output[-2] != 0 else 0
-        # stress_loss = output[2] / (2. * output[-1]) if output[-1] != 0 else 0
-        energy_loss = output[0] / (1. * len(batch))
-        force_loss  = output[1] / (1. * output[-2]) if output[-2] != 0 else 0
-        stress_loss = output[2] / (1. * output[-1]) if output[-1] != 0 else 0
+        energy_loss = output[0] / (2. * len(batch))
+        force_loss  = output[1] / (2. * output[-2]) if output[-2] != 0 else 0
+        stress_loss = output[2] / (2. * output[-1]) if output[-1] != 0 else 0
         
         energy_mae  = output[3] / len(batch)
         force_mae   = output[4] / output[-2] if output[-2] != 0 else 0
